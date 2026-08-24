7a47e6ff90dda045e0de02a0359489334be12055
jml <jean-marc.lienher@bluewin.ch>
2026-08-24 19:02:21 +0200

    multiboot

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -39,8 +39,35 @@ struct multiboot_info {
     uint32_t syms[4];
     uint32_t mmap_length;
     uint32_t mmap_addr;
-} PACKED;
 
+    uint32_t drives_length;
+    uint32_t drives_addr;
+    uint32_t config_table;
+    uint32_t boot_loader_name;
+    uint32_t apm_table;
+    uint32_t vbe_control_info;
+    uint32_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+    uint32_t framebuffer_addr_low;
+    uint32_t framebuffer_addr_hi;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t framebuffer_bpp;
+#define MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED 0
+#define MULTIBOOT_FRAMEBUFFER_TYPE_RGB     1
+#define MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT     2
+    uint8_t framebuffer_type;
+    uint8_t framebuffer_red_field_position;/*uint32_t framebuffer_palette_addr;*/
+    uint8_t framebuffer_red_mask_size;
+    uint8_t framebuffer_green_field_position;
+    uint8_t framebuffer_green_mask_size;
+    uint8_t framebuffer_blue_field_position;/*uint16_t framebuffer_palette_num_colors;*/
+    uint8_t framebuffer_blue_mask_size;
+};
 static void usb_worker_task(void *arg) {
     UNUSED(arg);
     while (1) {
@@ -69,10 +96,15 @@ void kmain(uint32_t magic, struct multiboot_info *mbi) {
     uint32_t mem_kb;
     phys_addr_t heap_mem;
     pci_device_t *xhci_dev;
+    uint32_t framebuffer_palette_addr;
+    uint16_t framebuffer_palette_num_colors;
 
     UNUSED(magic);
 
-    /* 1. Initialize Display and Serial Log */
+    framebuffer_palette_addr = *((uint32_t*)(&mbi->framebuffer_red_field_position));
+    framebuffer_palette_num_colors = *((uint16_t*)(&mbi->framebuffer_blue_field_position));
+ 
+   /* 1. Initialize Display and Serial Log */
     vga_init();
 
     kprint_color(0x0E, "\n=======================================================\n");
